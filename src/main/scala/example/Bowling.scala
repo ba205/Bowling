65df63fe65c24cdbe36d3a3e496f0de647269f0b
ba205 <basilahmad25@yahoo.com>
2018-01-22 19:10:34 -0500

    Enumerated possible correct values for one frame

--- src/main/scala/example/Bowling.scala
+++ src/main/scala/example/Bowling.scala
@@ -8,7 +8,24 @@ trait ComputeScore {
  
   lazy val greeting: String = "hello"
 
+  lazy val strike : String = "X"
+  lazy val spare : String = "/"
+  lazy val miss : String = "-"
+  lazy val digits : Array[String] = Array("1", "2", "3", "4", "5", "6", "7", "8","9")
+  lazy val digitMiss : Array[String] = digits.map(x => x + miss)
+  lazy val digitSpare : Array[String] = digits.map(x => x + spare)
+  lazy val digitDigit : Array[String] = Array("")
+  lazy val missMiss : String = miss + miss
+  lazy val missSpare : String = miss + spare
+  lazy val missDigit : Array[String] = digits.map(x => miss + x) 
+  lazy val digitSpareMiss : Array[String] = digits.map(x => x + spare + miss)
+  lazy val digitSpareDigit : Array[String] = Array("")
+  lazy val digitSpareStrike : Array[String] = digits.map(x => x + spare + strike)
+  lazy val missSpareMiss : String = miss + spare + miss
+  lazy val missSpareDigit : Array[String] = digits.map(x => miss + spare + x)
+  lazy val missSpareStrike : String = miss + spare + strike
+
   def printHello() {
-      println(greeting)
+      println(missSpareDigit.mkString(" "))
   }
 }
